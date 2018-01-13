6599ef09f5d7d1c85c8214d6e19de471dde50ee7
Jervon Arnold <jervonjarnold@gmail.com>
2018-01-13 15:52:54 -0500

    added implicit intents to call business number and open up google maps

--- app/src/main/java/nyc/c4q/foodsearch/recycleview/SavedRecycleView.java
+++ app/src/main/java/nyc/c4q/foodsearch/recycleview/SavedRecycleView.java
@@ -1,6 +1,8 @@
 package nyc.c4q.foodsearch.recycleview;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,15 +36,34 @@ public class SavedRecycleView extends RecyclerView.Adapter<SavedRecycleView.Test
     }
 
     @Override
-    public void onBindViewHolder(SavedRecycleView.Test_Holder holder, int position) {
-        Business business = businessList.get(position);
+    public void onBindViewHolder(final SavedRecycleView.Test_Holder holder, int position) {
+        final Business business = businessList.get(position);
         Picasso.with(holder.context)
                 .load(business.getImage_url())
                 .fit()
                 .into(holder.imageView);
 
         holder.address.setText(business.getLocation().getDisplay_address().get(0) + business.getLocation().getDisplay_address().get(1));
+        holder.address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri myCord = Uri.parse("geo:" + business.getCoordinates().getLatitude() + "," + business.getCoordinates().getLongitude());
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, myCord);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                holder.context.startActivity(mapIntent);
+
+            }
+        });
+
         holder.phoneNum.setText(business.getDisplay_phone());
+        holder.phoneNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent call = new Intent(Intent.ACTION_DIAL);
+                call.setData(Uri.parse("tel:" + business.getDisplay_phone()));
+                holder.context.startActivity(call);
+            }
+        });
         holder.businessName.setText(business.getName());
     }
 
