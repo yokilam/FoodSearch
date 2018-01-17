3a807d778ee592bdc3caab8d8c57dd69f933ffeb
Jervon Arnold <jervonjarnold@gmail.com>
2018-01-17 11:18:12 -0500

    added animation to heart buttion in item viewe xml

--- app/src/main/java/nyc/c4q/foodsearch/recycleview/BusinessViewHolder.java
+++ app/src/main/java/nyc/c4q/foodsearch/recycleview/BusinessViewHolder.java
@@ -1,20 +1,25 @@
 package nyc.c4q.foodsearch.recycleview;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.sackcentury.shinebuttonlib.ShineButton;
 import com.squareup.picasso.Picasso;
 
 import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 import nyc.c4q.foodsearch.mode.view.Business;
 import nyc.c4q.foodsearch.R;
 
+import static android.content.ContentValues.TAG;
 import static android.content.Context.MODE_PRIVATE;
 
 /**
@@ -30,7 +35,9 @@ public class BusinessViewHolder extends RecyclerView.ViewHolder {
     private SharedPreferences log;
     private static final String SHARED_PREF_KEY = "MY_SAVED_LIST";
     private SharedPreferences.Editor editor;
-    private Button button;
+//    private Button button;
+    Context context;
+    ShineButton shineButton;
 
 
     public BusinessViewHolder(View itemView) {
@@ -41,20 +48,15 @@ public class BusinessViewHolder extends RecyclerView.ViewHolder {
         rating= itemView.findViewById(R.id.rating);
         category= itemView.findViewById(R.id.category);
         businesslogo= itemView.findViewById(R.id.business_image);
-        button=itemView.findViewById(R.id.button);
+//        button=itemView.findViewById(R.id.button);
+        shineButton = itemView.findViewById(R.id.po_image0);
+        context = itemView.getContext();
 
         log = itemView.getContext().getSharedPreferences(SHARED_PREF_KEY, MODE_PRIVATE);
         editor = log.edit();
     }
 
     public void onBind(final Business business) {
-        if (log.contains(business.getId())) {
-            button.setBackgroundResource(R.drawable.clicked_heart);
-        } else if (!log.contains(business.getId())) {
-            button.setBackgroundResource(R.drawable.heart);
-        }
-
-
         name.setText(business.getName());
         StringBuilder fulladdress= new StringBuilder();
 
@@ -71,20 +73,42 @@ public class BusinessViewHolder extends RecyclerView.ViewHolder {
                 .fit()
                 .into(businesslogo);
 
-        button.setOnClickListener(new View.OnClickListener() {
+
+
+        ShineButton shineButtonJava = new ShineButton(context.getApplicationContext());
+
+        shineButtonJava.setBtnColor(Color.GRAY);
+        shineButtonJava.setBtnFillColor(Color.RED);
+        shineButtonJava.setShapeResource(R.raw.heart);
+        shineButtonJava.setAllowRandomColor(true);
+        shineButtonJava.setShineSize(50);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(50, 50);
+        shineButtonJava.setLayoutParams(layoutParams);
+
+
+        if (log.contains(business.getId())) {
+            shineButton.setChecked(true);
+        } else if (!log.contains(business.getId())) {
+           shineButton.setChecked(false);
+        }
+        shineButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 if (!log.contains(business.getId())) {
-                    button.setBackgroundResource(R.drawable.clicked_heart);
                     Gson gson = new Gson();
                     String json = gson.toJson(business);
                     editor.putString(business.getId(), json);
                     editor.commit();
                 } else if (log.contains(business.getId())) {
                     editor.remove(business.getId()).commit();
-                    button.setBackgroundResource(R.drawable.heart);
                 }
             }
         });
+
+
+
+
+
+
     }
 }
